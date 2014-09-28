ae8326185fd67d3906b07277f1164325c3eb65fe
ehret_g <guillaume.ehret@gmail.com>
2014-09-28 16:54:17 +0200

    Add filter to print the number of actives elements

--- confman-web/app/js/controller/applicationdetail.js
+++ confman-web/app/js/controller/applicationdetail.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('confman').controller('applicationDetailCtrl', function ($rootScope, $scope, $modal, $routeParams, Application, Environment, SoftwareSuite, Instance, $location) {
+angular.module('confman').controller('applicationDetailCtrl', function ($rootScope, $scope, $modal, $routeParams, $filter, Application, Environment, SoftwareSuite, Instance, $location) {
 
     //Page definition
     $rootScope.currentPage = {
@@ -8,8 +8,8 @@ angular.module('confman').controller('applicationDetailCtrl', function ($rootSco
         name: 'Application',
         description: $routeParams.id > 0 ? 'Update Application' : 'Create new application',
         icon: 'ic_settings_24px',
-        actionbar : [
-            {icon: 'ic_arrow_back_24px' , action : function(){
+        actionbar: [
+            {icon: 'ic_arrow_back_24px', action: function () {
                 $location.path('/application');
             }}
         ]
@@ -22,9 +22,15 @@ angular.module('confman').controller('applicationDetailCtrl', function ($rootSco
     //Type de parameters
     $scope.parametertypes = ['APPLICATION', 'INSTANCE'];
 
+    var refreshActiveDependencies = function () {
+        $scope.instancesize = $filter('filter')($scope.application.instances, {active: true}).length;
+        $scope.versionsize = $filter('filter')($scope.application.versions, {active: true}).length;
+        $scope.paramsize = $filter('filter')($scope.application.parameters, {active: true}).length;
+    }
+
     //Load environments
     if ($routeParams.id > 0) {
-        $scope.application = Application.get({id: $routeParams.id}, function (app) {
+        Application.get({id: $routeParams.id}, function (app) {
             angular.forEach(app.instances, function (elt) {
                 angular.forEach($scope.environments, function (env) {
                     if (elt.idEnvironment === env.id) {
@@ -32,7 +38,10 @@ angular.module('confman').controller('applicationDetailCtrl', function ($rootSco
                     }
                 });
             });
+            $scope.application = app;
+            refreshActiveDependencies();
         });
+
     }
     else {
         $scope.application = {};
@@ -74,7 +83,7 @@ angular.module('confman').controller('applicationDetailCtrl', function ($rootSco
 
         var callBackCreateInstance = function (data) {
             $scope.error = null;
-            data.active=true;
+            data.active = true;
             if (!$scope.application.instances) {
                 $scope.application.instances = [];
             }
@@ -85,18 +94,19 @@ angular.module('confman').controller('applicationDetailCtrl', function ($rootSco
             });
             //We see if the cpde exist
             var find = false;
-            angular.forEach($scope.application.instances,function(elt){
-                if(data.code===elt.code){
-                    elt.code=data.code;
-                    elt.label=data.label;
-                    elt.idEnvironment=data.idEnvironment;
-                    elt.active=true;
-                    find=true;
+            angular.forEach($scope.application.instances, function (elt) {
+                if (data.code === elt.code) {
+                    elt.code = data.code;
+                    elt.label = data.label;
+                    elt.idEnvironment = data.idEnvironment;
+                    elt.active = true;
+                    find = true;
                 }
             });
-            if(!find) {
+            if (!find) {
                 $scope.application.instances.push(data);
             }
+            refreshActiveDependencies();
         }
     };
 
@@ -133,24 +143,25 @@ angular.module('confman').controller('applicationDetailCtrl', function ($rootSco
 
         var callBackCreateParameter = function (data) {
             $scope.error = null;
-            data.active=true;
+            data.active = true;
             if (!$scope.application.parameters) {
                 $scope.application.parameters = [];
             }
             //We see if the cpde exist
             var find = false;
-            angular.forEach($scope.application.parameters,function(elt){
-                if(data.code===elt.code){
-                    elt.code=data.code;
-                    elt.label=data.label;
-                    elt.type=data.type;
-                    elt.active=true;
-                    find=true;
+            angular.forEach($scope.application.parameters, function (elt) {
+                if (data.code === elt.code) {
+                    elt.code = data.code;
+                    elt.label = data.label;
+                    elt.type = data.type;
+                    elt.active = true;
+                    find = true;
                 }
             });
-            if(!find) {
+            if (!find) {
                 $scope.application.parameters.push(data);
             }
+            refreshActiveDependencies();
         }
     };
 
@@ -186,37 +197,38 @@ angular.module('confman').controller('applicationDetailCtrl', function ($rootSco
 
         var callBackCreateVersion = function (data) {
             $scope.error = null;
-            data.active=true;
+            data.active = true;
             if (!$scope.application.versions) {
                 $scope.application.versions = [];
             }
             //We see if the cpde exist
             var find = false;
-            angular.forEach($scope.application.versions,function(elt){
-                if(data.code===elt.code){
-                    elt.code=data.code;
-                    elt.label=data.label;
-                    elt.active=true;
-                    find=true;
+            angular.forEach($scope.application.versions, function (elt) {
+                if (data.code === elt.code) {
+                    elt.code = data.code;
+                    elt.label = data.label;
+                    elt.active = true;
+                    find = true;
                 }
             });
-            if(!find) {
+            if (!find) {
                 $scope.application.versions.push(data);
             }
+            refreshActiveDependencies();
         }
     };
 
     //Save application
     //----------------------------
-    $scope.save = function(){
+    $scope.save = function () {
 
-        if(!$scope.application.id){
-            Application.save($scope.application, function (data){
+        if (!$scope.application.id) {
+            Application.save($scope.application, function (data) {
                 $scope.application = data;
-                $rootScope.error=null;
+                $rootScope.error = null;
             }, $scope.callbackKO);
         }
-        else{
+        else {
             $scope.application.$update(function (data) {
                     $scope.application = data;
                     $rootScope.error = null;
@@ -228,7 +240,7 @@ angular.module('confman').controller('applicationDetailCtrl', function ($rootSco
 
     //Delete application
     //----------------------------
-    $scope.delete = function(elt, $event){
+    $scope.delete = function (elt, $event) {
         var modalInstance = $modal.open({
             templateUrl: 'modalConfirmDelete.html',
             controller: function ($scope, $modalInstance, entity_todelete) {
@@ -241,7 +253,7 @@ angular.module('confman').controller('applicationDetailCtrl', function ($rootSco
                 };
             },
             resolve: {
-                entity_todelete : function () {
+                entity_todelete: function () {
                     return 'application ' + elt.code;
                 }
             }
@@ -251,24 +263,25 @@ angular.module('confman').controller('applicationDetailCtrl', function ($rootSco
             $event.stopPropagation();
             Application.delete(
                 elt,
-                function(data){
+                function (data) {
                     $location.path('/application');
                 },
                 $scope.callbackKO);
         });
     }
-    $scope.cancel = function(){
+    $scope.cancel = function () {
         $location.path('/application');
     }
 
     //Delete some dependencies
     //----------------------------
-    var deleteEntities = function($modal, entities, nameentities, callback){
+    var deleteEntities = function ($modal, entities, nameentities, callback) {
         var modalInstance = $modal.open({
             templateUrl: 'modalConfirmDelete.html',
             controller: function ($scope, $modalInstance, entity_todelete) {
                 $scope.entity_todelete = entity_todelete;
                 $scope.ok = function () {
+                    refreshActiveDependencies();
                     $modalInstance.close(true);
                 };
                 $scope.cancel = function () {
@@ -276,53 +289,53 @@ angular.module('confman').controller('applicationDetailCtrl', function ($rootSco
                 };
             },
             resolve: {
-                entity_todelete : function () {
+                entity_todelete: function () {
                     return nameentities + ' selected';
                 }
             }
         });
         //callback dans lequel on fait la suppression
         modalInstance.result.then(function () {
-            callback(entities.filter(function(elt){
+            callback(entities.filter(function (elt) {
                 return !elt.deleted;
             }));
         });
     }
-    $scope.deleteInstance = function(){
-        deleteEntities($modal, $scope.application.instances, 'instances', function(liste){
+    $scope.deleteInstance = function () {
+        deleteEntities($modal, $scope.application.instances, 'instances', function (liste) {
             $scope.application.instances = liste;
         });
     }
-    $scope.chgInstanceToDelete = function(){
-        $scope.nbInstanceToDelete=0;
-        $scope.application.instances.forEach(function(elt){
-            if(elt.deleted){
+    $scope.chgInstanceToDelete = function () {
+        $scope.nbInstanceToDelete = 0;
+        $scope.application.instances.forEach(function (elt) {
+            if (elt.deleted) {
                 $scope.nbInstanceToDelete++;
             }
         })
     }
-    $scope.deleteVersion = function(){
-        deleteEntities($modal, $scope.application.versions, 'versions', function(liste){
+    $scope.deleteVersion = function () {
+        deleteEntities($modal, $scope.application.versions, 'versions', function (liste) {
             $scope.application.versions = liste;
         });
     }
-    $scope.chgVersionToDelete = function(){
-        $scope.nbVersionToDelete=0;
-        $scope.application.versions.forEach(function(elt){
-            if(elt.deleted){
+    $scope.chgVersionToDelete = function () {
+        $scope.nbVersionToDelete = 0;
+        $scope.application.versions.forEach(function (elt) {
+            if (elt.deleted) {
                 $scope.nbVersionToDelete++;
             }
         })
     }
-    $scope.deleteParameter = function(){
-        deleteEntities($modal, $scope.application.parameters, 'parameters', function(liste){
+    $scope.deleteParameter = function () {
+        deleteEntities($modal, $scope.application.parameters, 'parameters', function (liste) {
             $scope.application.parameters = liste;
         });
     }
-    $scope.chgParameterToDelete = function(){
-        $scope.nbParameterToDelete=0;
-        $scope.application.parameters.forEach(function(elt){
-            if(elt.deleted){
+    $scope.chgParameterToDelete = function () {
+        $scope.nbParameterToDelete = 0;
+        $scope.application.parameters.forEach(function (elt) {
+            if (elt.deleted) {
                 $scope.nbParameterToDelete++;
             }
         })
